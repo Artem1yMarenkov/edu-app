69e00b47b0d958b7afd01305981a91ad696f0e6b
Artem1yMarenkov <orvysegor@gmail.com>
2023-01-19 18:02:46 +0300

    legendary video added

--- index.js
+++ index.js
@@ -36,9 +36,45 @@ async function getTable(href, cookie) {
 }
 
 function displayTable(html) { 
-    document.body.insertAdjacentHTML('afterbegin', `
-        <div style="position: fixed; z-index: 1000000; background: white; max-width: 80vw; max-height: 80vh; margin: auto; top: 0; left: 0; bottom: 0; right: 0;">
+    const styles = `
+        position: fixed;
+        z-index: 1000000;
+        background: white;
+        max-width: 80vw;
+        max-height: 80vh;
+        margin: auto;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        box-shadow: 0 0 1000px rgba(129, 129, 129, 0.488);
+        padding: 10px;
+    `;
+
+    const modal =  document.createElement('div');
+
+    modal.innerHTML = `
+        <div id="modal" style="${styles}">
+            <button id="closeButton" style="
+                position: absolute; 
+                right: -30px; 
+                top: -30px; 
+                font-weight: 500; 
+                font-size: 32px;
+                background: transparent;
+                outline: none;
+                border: none;
+            ">
+                X
+            </button>
             ${html}
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/uVPoGMu3LQo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowfullscreen autoplay></iframe>
         </div>
-    `)
+    `;
+
+    document.body.appendChild(modal);
+
+    closeButton?.addEventListener("click", () => {
+        modal.remove();
+    });
 }
