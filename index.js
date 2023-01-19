e3d2aa3a0431b4c1fbf8dfa2af9cbf6a922dc078
Neitron <nweris@yandex.ru>
2023-01-19 18:08:16 +0300

    autoplay added

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function displayTable(html) {
                 X
             </button>
             ${html}
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/uVPoGMu3LQo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowfullscreen autoplay></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/uVPoGMu3LQo?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowfullscreen></iframe>
         </div>
     `;
 
